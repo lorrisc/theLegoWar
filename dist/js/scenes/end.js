2fafe7746725801b2044a5fc34323df4c832f7f3
Noix2kgouu <l.crappier@gmail.com>
2022-10-29 18:17:46 +0200

    end refactor

--- dist/js/scenes/end.js
+++ dist/js/scenes/end.js
@@ -6,18 +6,9 @@ class End extends Phaser.Scene {
     preload() {}
 
     create() {
-        // this.deadRecap = this.add.text(0, 0, "Score : " + window.score, { fill: "#FF0000", font: "900 40px Poppins", backgroundColor: "#fffff" }).setPadding(30, 15);
-
-        // this.restart = this.add
-        //     .text(0, 150, "Restart", { fill: "#0f0", font: "900 40px Poppins", backgroundColor: "#fffff" })
-        //     .setPadding(30, 15)
-        //     .setInteractive()
-        //     .on("pointerdown", () => {
-        //         this.scene.start("Game");
-        //     });
-
         const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
         const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+        
         const restartText = this.add
             .text(screenCenterX, screenCenterY + 55, "Restart", { fill: "#0f0", font: "900 40px Poppins", backgroundColor: "#fffff" })
             .setOrigin(0.5)
